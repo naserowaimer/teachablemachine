4c7a0197eb72997b6ac0f8be47b4a6dc505310dc
naserowaimer <32237278+naserowaimer@users.noreply.github.com>
2024-11-13 14:48:10 +0300

    Enhance image classifier UI and functionality with loading indicators and improved layout.

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,8 @@
 const URL = 'https://teachablemachine.withgoogle.com/models/cx3R3CUXZ/';
 
+const loadingStatus = document.querySelector('.loadingStatus .loadingConitainer');
+const uploadedImage = document.querySelector('#uploaded-image');
+
 let model, labelContainer, maxPredictions;
 
 async function init() {
@@ -11,9 +14,9 @@ async function init() {
     maxPredictions = model.getTotalClasses();
 
     // append elements to the DOM
-    labelContainer = document.getElementById('label-container');
+    labelContainer = document.querySelector('#label-container .indecator');
     for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement('div'));
+        labelContainer.appendChild(document.createElement('div')).innerHTML = '0%';
     }
 }
 
@@ -22,19 +25,28 @@ async function predict(imageElement) {
     const prediction = await model.predict(imageElement, false);
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction =
-            prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerHTML = classPrediction;
+            prediction[i].className + ': ' + (prediction[i].probability*100).toFixed(2) + '%';
+        
+        labelContainer.querySelectorAll('div')[i].innerHTML = classPrediction;
+        labelContainer.querySelectorAll('div')[i].style.width = `${prediction[i].probability.toFixed(2) * 100}%`;
+        loadingStatus.style.display = 'none';
     }
 }
 
 document.getElementById('image-upload').addEventListener('change', function(event) {
+    document.getElementById('file-name').textContent = this.files[0].name;;
+    loadingStatus.style.display = 'block';
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = function() {
         const image = new Image();
+        uploadedImage.src = reader.result;
+        uploadedImage.style.display = 'block';
         image.src = reader.result;
         image.onload = function() {
-            predict(image);
+            setTimeout(() => {
+                predict(image);
+            }, 100);
         };
     };
     reader.readAsDataURL(file);
